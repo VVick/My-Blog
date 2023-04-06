db4dbaf0c8d548e1394a76c2b6f4ec4bc978c3f1
oyc <1051135835@qq.com>
2023-04-06 20:37:22 +0800

    Changes

--- src/test/java/com/site/blog/my/core/test/test.java
+++ src/test/java/com/site/blog/my/core/test/test.java
@@ -10,5 +10,6 @@ package com.site.blog.my.core.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("this is version 1");
+        System.out.println("this is version 2");
     }
 }
