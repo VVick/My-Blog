b2f6e5f13ea7cdf4625efa3e9fc5226bc861e573
oyc <1051135835@qq.com>
2023-04-06 20:52:37 +0800

    Changes

--- src/test/java/com/site/blog/my/core/test/test.java
+++ src/test/java/com/site/blog/my/core/test/test.java
@@ -11,5 +11,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("this is version 1");
         System.out.println("this is version 2");
+        System.out.println("hot fix hot fix hot fix hot fix");
     }
 }
