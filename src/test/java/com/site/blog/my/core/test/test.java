d7d013e6c6f274390f42177540a29e265da4f2ca
oyc <1051135835@qq.com>
2023-04-06 20:35:52 +0800

    Changes

---
+++ src/test/java/com/site/blog/my/core/test/test.java
@@ -0,0 +1,14 @@
+package com.site.blog.my.core.test;
+
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author OuYangCong
+ * @Date 2023/4/6
+ * @Version 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this is version 1");
+    }
+}
